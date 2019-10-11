9070cef96103fd49dd83fbcb2cefa0b9c995e52d
Nat McConnaughay <natmcconnaughay@gmail.com>
2019-10-11 03:57:43 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -16,8 +16,8 @@ def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     p "I LOVE YOU TOO PUMPKIN!"
   elsif phrase.upcase == phrase
-    p "HUH?! SPEAK UP, SONNY!"
-  else
     p "NO, NOT SINCE 1938!"
+  else
+    p "HUH?! SPEAK UP, SONNY!"
   end
 end
