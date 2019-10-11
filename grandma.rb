275c07704a9f876e5a96e5a56c69b72bb4dfda0e
Nat McConnaughay <natmcconnaughay@gmail.com>
2019-10-11 03:45:50 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -11,3 +11,12 @@
 
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
+
+def speak_to_grandma(phrase)
+  if phrase == "I LOVE YOU GRANDMA!"
+    p "I LOVE YOU TOO PUMPKIN!"
+  elsif phrase.upcase == phrase
+    p "HUH?! SPEAK UP, SONNY!"
+  else
+    p "NO, NOT SINCE 1938!"
+end
